2c1525b912bafb817c9b73275b13e9ad7762a3bc
oleeriksen <oleeriksen1958@gmail.com>
2025-08-06 13:17:10 +0200

    Update Config.cs

--- indexer/Config.cs
+++ indexer/Config.cs
@@ -2,5 +2,7 @@ namespace Indexer;
 
 public class Config
 {
+    // the folder to be indexed - all .txt files in that folder (and subfolders)
+    // will be indexed
     public static string FOLDER = @"/Users/oleeriksen/Data/seData/medium";
 }

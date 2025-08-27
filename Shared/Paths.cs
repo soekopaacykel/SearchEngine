860679354082f81651816f59b1ab2c73e3a6ac93
oleeriksen <oleeriksen1958@gmail.com>
2025-08-27 16:04:23 +0200

    div

--- Shared/Paths.cs
+++ Shared/Paths.cs
@@ -3,6 +3,7 @@ namespace Shared
 {
     public class Paths
     {
-        public static string DATABASE = @"/Users/oleeriksen/Data/searchDBmedium.db";
+        //public static string DATABASE = @"/Users/oleeriksen/Data/searchDBmedium.db";
+        public static string DATABASE = "Server=127.0.0.1:5432;User Id=oleeriksen;Password=1234;Database=oleeriksen;";
     }
 }

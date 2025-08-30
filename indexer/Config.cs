789d6fe4e69146a6c053ae2e0473edf295612efb
oleeriksen <oe@easv.dk>
2025-08-30 14:44:15 +0200

    hot fix

--- indexer/Config.cs
+++ indexer/Config.cs
@@ -4,5 +4,5 @@ public class Config
 {
     // the folder to be indexed - all .txt files in that folder (and subfolders)
     // will be indexed
-    public static string FOLDER = @"/Users/oleeriksen/Data/seData/medium";
+    public static string FOLDER = @"/Users/ole/Data/seData/medium2";
 }

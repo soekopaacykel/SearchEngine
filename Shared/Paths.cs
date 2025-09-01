c0087146e9db8b7bec87f3e5ad0e5d50d62ddebc
Amelia Valentin <eaaalov@students.eaaa.dk>
2025-09-01 16:57:59 +0200

    Sqlite setup

--- Shared/Paths.cs
+++ Shared/Paths.cs
@@ -3,7 +3,7 @@ namespace Shared
 {
     public class Paths
     {
-        public static readonly string SQLITE_DATABASE = @"/Users/oleeriksen/Data/searchDBmedium.db";
+        public static readonly string SQLITE_DATABASE = @"C:\Users\Amelia\Desktop\skole\SearchEngine\DB\DB.db";
         public static readonly string POSTGRES_DATABASE = "Server=127.0.0.1:5432;User Id=oleeriksen;Password=1234;database=search";
     }
 }
